379b1772f35cc9990bae59ad4ea5dd7a55a2b833
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-04 00:10:17 -0300

    Create: implements - PlaylistService.findAll (PlaylistServiceImpl)

--- src/main/kotlin/com/example/playlist/service/PlaylistServiceImpl.kt
+++ src/main/kotlin/com/example/playlist/service/PlaylistServiceImpl.kt
@@ -3,6 +3,7 @@ package com.example.playlist.service
 import com.example.playlist.document.Playlist
 import com.example.playlist.repository.PlaylistRepository
 import org.springframework.beans.factory.annotation.Autowired
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 class PlaylistServiceImpl: PlaylistService {
@@ -11,4 +12,6 @@ class PlaylistServiceImpl: PlaylistService {
     val repository: PlaylistRepository = TODO();
 
     override fun findById(id: String): Mono<Playlist> { return repository.findById(id); }
+    override fun findAll(): Flux<Playlist> {return repository.findAll()}
+    
 }
