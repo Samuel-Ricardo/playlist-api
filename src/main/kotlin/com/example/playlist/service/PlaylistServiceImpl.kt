790549f627e9cd538c111a12b3610185c7bea9a9
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-04 00:08:17 -0300

    Create: implements - PlaylistService.findById (PlaylistServiceImpl)

--- src/main/kotlin/com/example/playlist/service/PlaylistServiceImpl.kt
+++ src/main/kotlin/com/example/playlist/service/PlaylistServiceImpl.kt
@@ -1,5 +1,14 @@
 package com.example.playlist.service
 
+import com.example.playlist.document.Playlist
+import com.example.playlist.repository.PlaylistRepository
+import org.springframework.beans.factory.annotation.Autowired
+import reactor.core.publisher.Mono
+
 class PlaylistServiceImpl: PlaylistService {
 
+    @Autowired
+    val repository: PlaylistRepository = TODO();
+
+    override fun findById(id: String): Mono<Playlist> { return repository.findById(id); }
 }
