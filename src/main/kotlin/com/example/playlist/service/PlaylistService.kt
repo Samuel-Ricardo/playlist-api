b73b6afe0ff0f386f55aec8d5dfe928b1575cc3a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-03 23:56:19 -0300

    Create: fun - findAll (./service/PlaylistService.kt)

--- src/main/kotlin/com/example/playlist/service/PlaylistService.kt
+++ src/main/kotlin/com/example/playlist/service/PlaylistService.kt
@@ -1,4 +1,9 @@
 package com.example.playlist.service
 
+import com.example.playlist.document.Playlist
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
 interface PlaylistService {
+    fun findAll(): Flux<Playlist>
 }
