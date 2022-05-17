4478c2a1098d163d4c9311c40ba5a49784027334
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-17 09:01:51 -0300

    Update: Interface - PlaylistService (.service)

--- src/main/kotlin/com/example/playlist/service/PlaylistService.kt
+++ src/main/kotlin/com/example/playlist/service/PlaylistService.kt
@@ -1,9 +1,11 @@
 package com.example.playlist.service
 
 import com.example.playlist.document.Playlist
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+@Service
 interface PlaylistService {
     fun findAll(): Flux<Playlist>
     fun findById(id: String): Mono<Playlist>
