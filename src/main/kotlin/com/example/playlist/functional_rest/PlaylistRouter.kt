56b15cc80f6c2a1480e0e5a5c2f9e95faad8b805
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-17 09:13:52 -0300

    Create: Class - PlaylistRouter (.functional_rest)

---
+++ src/main/kotlin/com/example/playlist/functional_rest/PlaylistRouter.kt
@@ -0,0 +1,15 @@
+package com.example.playlist.functional_rest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RequestPredicates.*
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+
+@Configuration
+class PlaylistRouter {
+
+
+}
