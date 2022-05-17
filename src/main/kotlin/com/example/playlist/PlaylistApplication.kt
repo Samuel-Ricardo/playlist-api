b26cf332b71b676051e0aa88d04e209cd446f019
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-17 08:28:03 -0300

    Update: File - PlaylistHandler.kt (./)

--- src/main/kotlin/com/example/playlist/PlaylistApplication.kt
+++ src/main/kotlin/com/example/playlist/PlaylistApplication.kt
@@ -1,8 +1,14 @@
 package com.example.playlist
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.ComponentScans
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@ComponentScans
 @SpringBootApplication
 class PlaylistApplication
 
