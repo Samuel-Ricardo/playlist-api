c9f6986a5789116fdaf820db9d555c2170729ce7
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-17 09:22:24 -0300

    Create: Fun - getPlaylist (.controller.PlaylistController)

--- src/main/kotlin/com/example/playlist/controller/PlaylistController.kt
+++ src/main/kotlin/com/example/playlist/controller/PlaylistController.kt
@@ -1,9 +1,12 @@
 package com.example.playlist.controller
 
+import com.example.playlist.document.Playlist
 import com.example.playlist.service.PlaylistService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @CrossOrigin(origins = ["*"])
 @RestController
@@ -12,4 +15,6 @@ class PlaylistController(
     val service: PlaylistService
 ) {
 
+    @GetMapping(value = ["/playlist"])
+    fun getPlaylist(): Flux<Playlist> {return service.findAll()}
 }
