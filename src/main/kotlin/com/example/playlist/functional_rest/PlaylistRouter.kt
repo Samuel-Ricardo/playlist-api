59ec6d1f3602fa1534890df207de629d04ef8f77
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-05-17 09:14:16 -0300

    Create: Fun - route (.functional_rest.PlaylistRouter)

--- src/main/kotlin/com/example/playlist/functional_rest/PlaylistRouter.kt
+++ src/main/kotlin/com/example/playlist/functional_rest/PlaylistRouter.kt
@@ -10,6 +10,11 @@ import org.springframework.web.reactive.function.server.ServerResponse
 
 @Configuration
 class PlaylistRouter {
-
-
+    @Bean
+    fun route(handler: PlaylistHandler): RouterFunction<ServerResponse>{
+        return RouterFunctions
+            .route(GET("/playlist").and(accept(MediaType.APPLICATION_JSON)), handler::findAll)
+            .andRoute(GET("/playlist/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::findByID)
+            .andRoute(POST("/playlist").and(accept(MediaType.APPLICATION_JSON)), handler::save)
+    }
 }
